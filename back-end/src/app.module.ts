e53c49917b4888efffc4662f559f37b9e962f608
Boygioi15 <boygioi85@gmail.com>
2024-10-24 17:08:42 +0700

    Set up database connection and init user module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://boygioi85:Boypro12345@kq-shopdb.bipdj.mongodb.net/kq-shop?retryWrites=true&w=majority&appName=kq-shopDB',
+    ),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
